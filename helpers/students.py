05cb5b0603f8be8bf34575b2acc14fbc8a05aee9
Alex van Vliet <alex.van-vliet@epita.fr>
2019-05-13 21:22:51 +0200

    refactor(autocomplete): remove duplicates for all options

--- helpers/students.py
+++ helpers/students.py
@@ -1,6 +1,5 @@
 import os
 
-from helpers.autocomplete import get_arg_number
 from helpers.io import folder_exists, folder_ls
 from misc.config import STUDENTS_FOLDER
 
@@ -12,9 +11,3 @@ def get_downloaded_students(tp_slug):
     return [folder[len(tp_slug) + 1:]
             for folder in folder_ls(student_folder)
             if folder.startswith(tp_slug + "-")]
-
-def get_unused_downloaded_students(tp_slug, used):
-    students = get_downloaded_students(tp_slug)
-    for student in used:
-        students.remove(student)
-    return students
