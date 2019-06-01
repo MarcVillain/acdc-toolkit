0ea2fe8f758d46deffd07854e1abd8cdfa0148be
Marc Villain <marc.villain@epita.fr>
2019-06-01 12:33:13 +0200

    fix(list): should not list hidden files/folders

--- commands/list.py
+++ commands/list.py
@@ -11,7 +11,7 @@ def cmd_list(tp_slug):
     List all the downloaded repos
     :param tp_slug: TP slug
     """
-    for folder in folder_ls(STUDENTS_FOLDER):
+    for folder in folder_ls(STUDENTS_FOLDER, excludes=["\\..*"]):
         if tp_slug:
             if folder == tp_slug:
                 for subfolder in folder_ls(os.path.join(STUDENTS_FOLDER, folder)):
